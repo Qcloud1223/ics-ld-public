2fddbac9f3d3c45252f0c835f550fcdae58caeda
Yihan Dang <qcloud1014@gmail.com>
2024-04-14 16:29:27 +0800

    🎨 Consistent return code

--- src/resolve.cc
+++ src/resolve.cc
@@ -2,8 +2,9 @@
 
 #include <iostream>
 
-#define MULTIDEF 1
-#define NODEF 2
+#define FOUND_ALL_DEF 0
+#define MULTI_DEF 1
+#define NO_DEF 2
 
 std::string errSymName;
 
@@ -11,10 +12,10 @@ int callResolveSymbols(std::vector<ObjectFile> &allObjects);
 
 void resolveSymbols(std::vector<ObjectFile> &allObjects) {
     int ret = callResolveSymbols(allObjects);
-    if (ret == MULTIDEF) {
+    if (ret == MULTI_DEF) {
         std::cerr << "multiple definition for symbol " << errSymName << std::endl;
         abort();
-    } else if (ret == NODEF) {
+    } else if (ret == NO_DEF) {
         std::cerr << "undefined reference for symbol " << errSymName << std::endl;
         abort();
     }
@@ -30,5 +31,5 @@ int callResolveSymbols(std::vector<ObjectFile> &allObjects)
     // if found multiple definition, set the errSymName to problematic symbol name and return MULTIDEF;
     // if no definition is found, set the errSymName to problematic symbol name and return NODEF;
 
-    return 0;
+    return FOUND_ALL_DEF;
 }
